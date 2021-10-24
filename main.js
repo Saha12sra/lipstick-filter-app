3b7e0e28c0a73427ba495a6687a57e14b54a96d6
Saha12sra <74057039+Saha12sra@users.noreply.github.com>
2021-10-24 12:43:55 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,44 @@
-function preload(){
+noseX = 0;
+noseY = 0;
+function preload() {
+
+    lipstick = loadImage("https://i.postimg.cc/T1fZ46d5/lips4.png");
+}
+
+function setup() {
+
+    canvas = createCanvas(380, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size = (380, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 
 }
 
-function setup(){
-
-canvas= createCanvas(390, 310);
-canvas.center();
-video=createCapture(VIDEO);
-video.size=(390, 310);
-video.hide();
-poseNet=ml5.poseNet(video, modelLoaded);
-poseNet.on('pose', gotPoses);
-
+function modelLoaded() {
+    console.log('poseNet is intialized.')
 }
 
-function modelLoaded(){
-console.log('poseNet is intialized.')
-}
-
-function gotPoses(){
-    if(length.results>0){
+function gotPoses(results) {
+    if (results.length > 0) {
         console.log(results);
-        console.log("mouth x = "+results[0].pose.nose.x);
-        console.log("mouth y = "+results[0].pose.nose.y);
+        noseX= results[0].pose.nose.x-150;
+        noseY=results[0].pose.nose.y-75;
+        console.log("nose x = "+noseX);
+        console.log("nose y = "+noseY);
     }
 }
 
-function draw(){
+function draw() {
 
-image(video, 0, 0, 390, 310);
+    image(video, 0, 0, 380, 300);
+    image(lipstick, noseX, noseY, 40, 30);
 }
 
-function takeSnapshot(){
+function takeSnapshot() {
 
     save('Lipstick_filter_pic.jpeg');
-    
+
 }
